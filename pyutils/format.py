a45f955702f3fa4d73ccecdccbe04de85daae4c0
Henk van den Berg <henk.van.den.berg@dans.knaw.nl>
2018-04-23 16:01:29 +0200

    Format csv

---
+++ pyutils/format.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import csv
+
+
+class RFC4180(object):
+    delimiter = ','
+    quotechar = '"'
+    escapechar = None
+    doublequote = True
+    skipinitialspace = False
+    lineterminator = '\r\n'
+    quoting = csv.QUOTE_MINIMAL
