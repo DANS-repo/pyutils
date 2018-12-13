49b64fac17cc96fdc0a3ee36eaf584349ab0db2c
Henk van den Berg <henkvandenberg8@gmail.com>
2018-12-13 16:42:34 +0100

    format links

--- pyutils/format.py
+++ pyutils/format.py
@@ -12,3 +12,28 @@ class RFC4180(object):
     skipinitialspace = False
     lineterminator = '\r\n'
     quoting = csv.QUOTE_MINIMAL
+
+
+def link_easy(sid):
+    """
+    Creates an html link tag to a dataset page in Easy.
+
+    :param sid: a dataset id
+
+    :return: link to the page for that dataset
+    """
+    prefix = 'https://easy.dans.knaw.nl/ui/datasets/id/'
+    return '<a target="_blank" href="{}{}">{}</a>'.format(prefix, sid, sid)
+
+
+def link_fedora_file(sid):
+    """
+    Creates an html link tag to a file in Fedora.
+
+    :param sid: a file id
+
+    :return: link to the file content
+    """
+    return '<a href="http://easy01.dans.knaw.nl:8080/fedora/objects/{}/datastreams/EASY_FILE/content" target="_blank">{}</a>'\
+        .format(sid, sid)
+
